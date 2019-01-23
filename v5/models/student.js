e62560841e2924f56f2ad99256b803d198fcc425
Pratik Raj <pratik.raj0000@gmail.com>
2019-01-23 16:44:47 +0000

    web app

---
+++ v5/models/student.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var studentSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    fullname: String
+});
+
+studentSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("Student", studentSchema);
